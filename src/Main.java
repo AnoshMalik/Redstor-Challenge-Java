4c81b3f15e2960cbfac21ea561f8907df67e9e78
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-21 11:29:21 +0000

    Added products class. Product storage/retrieval not functional yet

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+       Product apple = new Product("Apple", 50);
+        System.out.println("WELCOME TO THE REDSTOR SUPERMARKET");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int appleCount = 5;
+         for(int i =1; i<appleCount+1;i++){
+             System.out.println("Apple - " + i + " @ £" + apple.getCost());
+         }
+
         }
     }
-}
