12c5f8c17037ff267c985818b69756e6e9367372
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-21 22:31:00 +0000

    Added methods to all car files. Checking whether methods/variables are better static or not

--- src/BMW.java
+++ src/BMW.java
@@ -1,2 +1,13 @@
 public class BMW {
+
+     final String name = "BMW";
+     final int cost = 30;
+
+    public  String getName(){
+        return name;
+    }
+
+    public int getCost(){
+        return cost;
+    }
 }
