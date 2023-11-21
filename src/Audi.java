f56af1b63fb85aae85e8612a89182891d8fe4d7e
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-21 16:27:13 +0000

    Created product class files and changed products class to interface

---
+++ src/Audi.java
@@ -0,0 +1,13 @@
+public class Audi implements Product {
+    String name = "Audi";
+    int cost = 60;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+}
--- src/Product.java
+++ src/Product.java
@@ -1,25 +1,7 @@
-public class Product {
+public interface Product {
 
-    private static String name = "";
-    private static int cost = 0;
-    public Product(String name, int cost ){
-    setName(this.name);
-    setCost(this.cost);
-    }
-
-    public void setName(String name){
-        name = this.name;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public void setCost(int cost){
-        cost = this.cost;
-    }
-    public int getCost(){
-        return cost;
-    }
-
+    public String getName();
+    public int getCost();
 
 
 }
