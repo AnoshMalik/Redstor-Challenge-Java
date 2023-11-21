4633affd0134a4abc961174fbdce3d7183f075f6
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-21 16:55:06 +0000

    Changed application logic to take in numbers as input

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,38 @@ public class Main {
         //Product apple = new Product();
         System.out.println("WELCOME TO THE REDSTOR SUPERCAR MARKET");
         System.out.println("SELECT YOUR CARS");
-        System.out.println("Type : A for Audi -  B for BMW - C for CHEVROLET - D for DODGE");
-
+        System.out.println("Type : 1 for Audi -  2 for BMW - 3 for CHEVROLET - 4 for DODGE OR 0 TO CLOSE APP");
         Scanner scanner = new Scanner(System.in);
-        String productName = scanner.next();
-        System.out.println(productName.toUpperCase());
+
+        boolean incomingInput = true;
+        while(incomingInput){
+            int productNumber = scanner.nextInt();
+            switch(productNumber){
+                case 0:
+                    System.out.println("Are you sure you wanna close app? 0 for YES - 1 for N)");
+                    int userChoice = scanner.nextInt();
+                    if(userChoice == 1){
+                        System.out.println("CLOSING APPLICATION");
+                        System.exit(0);
+                    }else{
+                        System.out.println("RESUMING APPLICATION");
+
+                    }
+                case 1 :
+                    System.out.println("You have selected Audi");
+                    System.out.printf("Enter Quantity - ");
+
+                    break;
 
 
+                case 2 : System.out.println("You have selected BMW"); break;
+                case 3 : System.out.println("You have selected Chevrolet");break;
+                case 4 : System.out.println("You have selected DODGE"); break;
 
-        if (productName.contains("audi")) {
-            System.out.println("You have  " + productName.toUpperCase() + ". Enter quantity");
 
-            int a = scanner.nextInt();
-            System.out.println("You entered - " + a);
-
-            float b = scanner.nextFloat();
-            System.out.println("You entered - " + b);
-        }else{
-            System.out.println("Product not recognised");
+            }
         }
+
+
     }
     }
